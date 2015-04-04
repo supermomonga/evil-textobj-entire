8f59de32e345de67e7f6b6081ead69b821a10972
Syohei YOSHIDA <syohex@gmail.com>
2015-04-04 13:58:00 +0900

    Fix for packaging
    - Evil dependency
    - Minimum Emacs version for using lexical-binding

--- evil-textobj-entire.el
+++ evil-textobj-entire.el
@@ -4,6 +4,7 @@
 
 ;; Author: supermomonga
 ;; Keywords: convenience, emulations
+;; Package-Requires: ((emacs "24") (evil "1.0.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
